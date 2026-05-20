18f043363705bc14c392465808a9be2a15999c09
MaxSharshon <sharshonmax@gmail.com>
2026-05-20 18:11:49 +0300

    feat(auth): add JWT token generation

---
+++ FinanceTracker.API/Services/JwtOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace FinanceTracker.API.Services;
+
+public class JwtOptions
+{
+    public string Secret { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public int ExpiresInMinutes { get; set; }
+}
---
+++ FinanceTracker.API/Services/JwtProvider.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using System.Text;
+using FinanceTracker.Core.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FinanceTracker.API.Services;
+
+public class JwtProvider(IOptions<JwtOptions> options)
+{
+    private readonly JwtOptions _jwtOptions = options.Value;
+    
+    public string Create(User user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Secret));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(
+            [
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            ]),
+            Expires = DateTime.UtcNow.AddMinutes(_jwtOptions.ExpiresInMinutes),
+            SigningCredentials = credentials,
+            Issuer = _jwtOptions.Issuer,
+            Audience = _jwtOptions.Audience
+        };
+
+        var handler = new JsonWebTokenHandler();
+
+        var token = handler.CreateToken(tokenDescriptor);
+
+        return token;
+    }
+}
